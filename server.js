59822f7330b0e8cc3f926757f9f742e282491475
ryo sato <ryos1988j19@gmail.com>
2025-02-02 10:29:48 +0900

    再調整中、上手くいかず

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-const path = require('path'); // path モジュールを最初にインポート
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+const path = require('path'); //path モジュールを最初にインポート
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 
 console.log('Supabase URL:', process.env.SUPABASE_URL);
 console.log('Supabase Key:', process.env.SUPABASE_ANON_KEY);
 
 const express = require('express');
-const cors = require('cors'); // CORSミドルウェアをインポート
+const cors = require('cors'); //CORSミドルウェアをインポート
 const app = express();
 
-// CORSを有効にする
+//CORSを有効にする
 app.use(cors());
 
-// 環境変数を使用してSupabaseクライアントを設定
+//環境変数を使用してSupabaseクライアントを設定
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_ANON_KEY;
 
-// スタティックファイルの提供
-app.use(express.static(path.resolve(__dirname, '../Booksinfo')));
+//静的ファイルを提供（ルートディレクトリから）
+app.use(express.static(path.join(__dirname)));
 
 app.get('/config', (req, res) => {
     const supabaseUrl = process.env.SUPABASE_URL;
